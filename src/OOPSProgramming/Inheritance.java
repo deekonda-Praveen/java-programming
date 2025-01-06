a7d5e3eb30b7dd0a1a79aa846dec184bb7168e8c
deekonda-praveen <deekondapraveen123@gmail.com>
2025-01-06 13:30:20 +0530

    6/1/2025

--- src/OOPSProgramming/Inheritance.java
+++ src/OOPSProgramming/Inheritance.java
@@ -13,13 +13,31 @@ class B extends A{
         System.out.println(b);
     }
 }
+//upto here one parent acquiring one child then single inheritance
+//now i am adding new child class as class C then this will be called as multi level inheritance
+class C extends B{
+    //here class B is having both A and B stuff and now if we extends B then that
+    //will be there inside the class C
+    int c;
+    void print(){
+        System.out.println(c);
+    }
+}
 public class Inheritance {
     public static void main(String[] args) {
         //just  create object for class B then use all the variable from both the classes
-        B bobj=new B();
+      /*  B bobj=new B();
         bobj.a=23;
         bobj.b=33;
         bobj.display();
         bobj.show();
+        */
+       C cobj=new C();
+       cobj.a=100;
+       cobj.b=200;
+       cobj.c=300;
+       cobj.display();
+       cobj.show();
+       cobj.print();
     }
 }
